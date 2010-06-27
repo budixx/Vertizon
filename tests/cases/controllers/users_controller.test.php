36eae5a8b4f59b008bc6db01aca88e2e9a2b9c51
Rudy <channel_life@yahoo.com>
2010-06-27 18:52:17 +0700

    baking tables for admin

---
+++ tests/cases/controllers/users_controller.test.php
@@ -0,0 +1,47 @@
+<?php
+/* Users Test cases generated on: 2010-06-27 18:06:54 : 1277639514*/
+App::import('Controller', 'Users');
+
+class TestUsersController extends UsersController {
+	var $autoRender = false;
+
+	function redirect($url, $status = null, $exit = true) {
+		$this->redirectUrl = $url;
+	}
+}
+
+class UsersControllerTestCase extends CakeTestCase {
+	var $fixtures = array('app.user');
+
+	function startTest() {
+		$this->Users =& new TestUsersController();
+		$this->Users->constructClasses();
+	}
+
+	function endTest() {
+		unset($this->Users);
+		ClassRegistry::flush();
+	}
+
+	function testAdminIndex() {
+
+	}
+
+	function testAdminView() {
+
+	}
+
+	function testAdminAdd() {
+
+	}
+
+	function testAdminEdit() {
+
+	}
+
+	function testAdminDelete() {
+
+	}
+
+}
+?>
---
+++ tests/fixtures/user_fixture.php
@@ -0,0 +1,22 @@
+<?php
+/* User Fixture generated on: 2010-06-27 18:06:49 : 1277639509 */
+class UserFixture extends CakeTestFixture {
+	var $name = 'User';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'username' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'password' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'username' => 'Lorem ipsum dolor sit amet',
+			'password' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+}
+?>
